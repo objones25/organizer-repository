a8895a39f7ad2a743156a68cd7b058bbef06dd60
Owen Jones <owenbeckettjones@gmail.com>
2020-04-16 11:20:10 -0700

    initial commit

--- homework organizer/ContentView.swift
+++ homework organizer/ContentView.swift
@@ -10,27 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selection = 0
- 
+    
+    
     var body: some View {
-        TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
+        ZStack{
+            TabView(selection: $selection){
+                CheckListVIew()
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "calendar.circle")
+                            Text("Homework Organizer")
+                        }
                 }
                 .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
+                NewAssignmentView()
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "folder.fill.badge.plus")
+                            Text("New Assignment")
+                        }
                 }
                 .tag(1)
+            }
         }
     }
 }
@@ -38,5 +39,77 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CheckListVIew: View {
+    @State private var assignments = 14
+    @State private var subject = ""
+    @State private var dueDate = ""
+    @State private var assignmentType = ""
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(spacing: 10){
+                ForEach(1..<assignments){
+                    Text("Priority \($0)")
+                    VStack {
+                        VStack{
+                            HStack{
+                                HStack {
+                                    Text("Subject:")
+                                    TextField("NA", text: self.$subject)
+                                }
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.1)
+                                HStack {
+                                    Text("Due Date:")
+                                    TextField("NA", text: self.$dueDate)
+                                }
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.1)
+                            }
+                            .padding(10)
+                            HStack{
+                                HStack{
+                                    Text("Assignment Type:")
+                                    TextField("NA", text: self.$assignmentType)
+                                }
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.1)
+                                HStack{
+                                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                        VStack{
+                                            Image(systemName: "pencil.and.ellipsis.rectangle")
+                                            Text("More Information")
+                                        }
+                                        
+                                    }
+                                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                        VStack{
+                                            Image(systemName: "pencil.slash")
+                                            Text("Complete Assignment")
+                                        }
+                                    }
+                                }
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.1)
+                            }
+                        }
+                    }
+                }
+            }
+            .background(Color.white)
+            .cornerRadius(15)
+            .padding(1)
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.black)
+    }
+}
+
+
+struct NewAssignmentView: View {
+    var body: some View {
+        Text("")
     }
 }
